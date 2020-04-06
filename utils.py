0757f8fde91fe3da0e80726bc544b2b4673ec0f4
moon_well <moon_well@live.cn>
2020-04-06 20:16:50 +0800

    fix

--- utils.py
+++ utils.py
@@ -4,12 +4,13 @@ Some codes from https://github.com/Newmu/dcgan_code
 from __future__ import division
 import math
 import pprint
-# import scipy.misc
-from PIL import Image
+import scipy.misc
 import numpy as np
 import copy
-from imageio import imread as _imread
-from imageio import imsave as _imsave
+try:
+    _imread = scipy.misc.imread
+except AttributeError:
+    from imageio import imread as _imread
 
 pp = pprint.PrettyPrinter()
 
@@ -43,7 +44,7 @@ class ImagePool(object):
 
 def load_test_data(image_path, fine_size=256):
     img = imread(image_path)
-    img = imgresize(img, (fine_size, fine_size))
+    img = scipy.misc.imresize(img, [fine_size, fine_size])
     img = img/127.5 - 1
     return img
 
@@ -51,10 +52,8 @@ def load_train_data(image_path, load_size=286, fine_size=256, is_testing=False):
     img_A = imread(image_path[0])
     img_B = imread(image_path[1])
     if not is_testing:
-        img_A = imgresize(img_A, (load_size, load_size))
-        # img_A = scipy.misc.imresize(img_A, [load_size, load_size])
-        img_B = imgresize(img_B, (load_size, load_size))
-        # img_B = scipy.misc.imresize(img_B, [load_size, load_size])
+        img_A = scipy.misc.imresize(img_A, [load_size, load_size])
+        img_B = scipy.misc.imresize(img_B, [load_size, load_size])
         h1 = int(np.ceil(np.random.uniform(1e-2, load_size-fine_size)))
         w1 = int(np.ceil(np.random.uniform(1e-2, load_size-fine_size)))
         img_A = img_A[h1:h1+fine_size, w1:w1+fine_size]
@@ -64,10 +63,8 @@ def load_train_data(image_path, load_size=286, fine_size=256, is_testing=False):
             img_A = np.fliplr(img_A)
             img_B = np.fliplr(img_B)
     else:
-        # img_A = scipy.misc.imresize(img_A, [fine_size, fine_size])
-        img_A = imgresize(img_A, (fine_size, fine_size))
-        # img_B = scipy.misc.imresize(img_B, [fine_size, fine_size])
-        img_B = imgresize(img_B, (fine_size, fine_size))
+        img_A = scipy.misc.imresize(img_A, [fine_size, fine_size])
+        img_B = scipy.misc.imresize(img_B, [fine_size, fine_size])
 
     img_A = img_A/127.5 - 1.
     img_B = img_B/127.5 - 1.
@@ -88,11 +85,7 @@ def imread(path, is_grayscale = False):
     if (is_grayscale):
         return _imread(path, flatten=True).astype(np.float)
     else:
-        return _imread(path, pilmode='RGB').astype(np.float)
-
-def imgresize(arr,size):
-    import numpy
-    return numpy.array(Image.fromarray(arr).resize(size))
+        return _imread(path, mode='RGB').astype(np.float)
 
 def merge_images(images, size):
     return inverse_transform(images)
@@ -108,7 +101,7 @@ def merge(images, size):
     return img
 
 def imsave(images, size, path):
-    return _imsave(path, merge(images, size))
+    return scipy.misc.imsave(path, merge(images, size))
 
 def center_crop(x, crop_h, crop_w,
                 resize_h=64, resize_w=64):
@@ -117,8 +110,8 @@ def center_crop(x, crop_h, crop_w,
   h, w = x.shape[:2]
   j = int(round((h - crop_h)/2.))
   i = int(round((w - crop_w)/2.))
-  return imgresize(
-      x[j:j+crop_h, i:i+crop_w], (resize_h, resize_w))
+  return scipy.misc.imresize(
+      x[j:j+crop_h, i:i+crop_w], [resize_h, resize_w])
 
 def transform(image, npx=64, is_crop=True, resize_w=64):
     # npx : # of pixels width/height of image
